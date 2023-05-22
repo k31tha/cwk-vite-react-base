cf0208436e200b541e8eeac39ffc65402043b0f5
k31tha <keith@kdasoft.co.uk>
2023-05-22 23:38:28 +0100

    Add basic test to prove testing works

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import {describe, it, expect} from 'vitest';
+
+describe('basic tests', () => {
+  it('true to be true', () => {
+    expect(true).toBe(true);
+  });
+  it('toUpperCase', () => {
+    const result = 'foobar'.toUpperCase();
+    expect(result).toMatchSnapshot();
+  });
+  it('toLowerCase - inline', () => {
+    const result = 'foobar'.toLowerCase();
+    expect(result).toMatchInlineSnapshot('"foobar"');
+  });
+});
